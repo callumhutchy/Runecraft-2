18143d42f3af4118c539a209baed4be9cb319210
callumhutchy <callumhutchie@hotmail.co.uk>
2015-08-28 01:47:10 +0100

    More changes

---
+++ src/main/java/com/callumhutchy/runecraft2/items/Sword.java
@@ -0,0 +1,10 @@
+package com.callumhutchy.runecraft2.items;
+
+public class Sword extends MartialWeapon{
+
+    public Sword(ToolMaterial material, String unlocalisedName) {
+	super(material, unlocalisedName);
+	// TODO Auto-generated constructor stub
+    }
+
+}
